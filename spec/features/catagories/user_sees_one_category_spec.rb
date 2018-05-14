d54fd5b264f46499e2e75073498e8899a4a1ded0
Manoj Panta <manojpanta95@gmail.com>
2018-05-13 20:46:54 -0600

    added test for back to categories from shoe page  of category

---
+++ spec/features/catagories/user_sees_one_category_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'User sees one category' do
+  scenario 'a user sees a category' do
+    category = Category.create(title: 'Finance')
+
+    visit category_path(category)
+
+    expect(current_path).to eq("/categories/#{category.id}")
+    expect(page).to have_content(category.title)
+  end
+
+  scenario 'a user can go back to  categories' do
+    category = Category.create(title: 'Finance')
+
+    visit category_path(category)
+    click_on '<<Back to categories'
+
+    expect(current_path).to eq(categories_path)
+  end
+end
