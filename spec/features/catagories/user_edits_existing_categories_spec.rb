90748100f6055b25fe9e4c2e2ec6e4020965a453
Manoj Panta <manojpanta95@gmail.com>
2018-05-13 20:12:10 -0600

    made it to work editing a ctegory

--- spec/features/catagories/user_edits_existing_categories_spec.rb
+++ spec/features/catagories/user_edits_existing_categories_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 describe 'user edits a existing category' do
   scenario 'user can edit a category' do
+    category = Category.create!(title: 'Education')
+    visit edit_category_path(category)
+
+
+    fill_in 'category[title]', with: 'Finance'
+    click_on 'Update'
+
+    expect(current_path).to eq(category_path(category))
+    expect(page).to have_content('Finance')
 
   end
 
