a7995166f95e708d469af45b87164e8c863c1b1d
Manoj Panta <manojpanta95@gmail.com>
2018-05-16 08:13:08 -0600

    set dashboard path to go to dashboard show

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -21,6 +21,7 @@ class CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
     @contact = Contact.new
+    @company_contacts = @company.contacts
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :jobs, only: [:index] do
     resources :comments
   end
-
+  get '/dashboard', to: 'dashboard#show', as:'dashboard'
   root to: 'dashboard#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
