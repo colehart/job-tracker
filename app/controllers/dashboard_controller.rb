1803c16dc2200b08d65a037acc42c07b9ad798f4
Manoj Panta <manojpanta95@gmail.com>
2018-05-15 23:12:24 -0600

    done with all the user stories

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def show
     @cities = Job.count_by_city
     @top_jobs = Job.count_by_interest
+    @top_companies = Company.top_companies_by_interest
   end
 end
--- spec/features/dashboard/user_can_see_dashboard_spec.rb
+++ spec/features/dashboard/user_can_see_dashboard_spec.rb
@@ -39,7 +39,6 @@ describe 'when user visits dashboard' do
                        category: category)
 
     visit root_path
-    save_and_open_page
     high_interest_job_count = '1 jobs'
     medium_interest_job_count = '3 jobs'
     low_interest_job_count = '1 jobs'
@@ -79,5 +78,70 @@ describe 'when user visits dashboard' do
     expect(page).to have_link(seattle_jobs_link)
     expect(page).to have_content(denver_jobs_count)
     expect(page).to have_content(seattle_jobs_count)
+  end
+  scenario 'a user can see jobs by location with count' do
+    company1 = Company.new(name: 'Turing')
+    company2 = Company.new(name: 'Amazon')
+    company3 = Company.new(name: 'Apple')
+    category = Category.create!(title: 'Information')
+    #this is for turing company
+    job1 = Job.create!(title: 'Developer',
+                       level_of_interest: 40,
+                       city: 'Denver',
+                       company: company1,
+                       category: category)
+    job2 = Job.create!(title: 'Developer',
+                       level_of_interest: 40,
+                       city: 'Denver',
+                       company: company1,
+                       category: category)
+    job3 = Job.create!(title: 'Developer',
+                       level_of_interest: 40,
+                       city: 'Seattle',
+                       company: company1,
+                       category: category)
+    # these three is for amazon
+    job4 = Job.create!(title: 'Developer',
+                       level_of_interest: 50,
+                       city: 'Denver',
+                       company: company2,
+                       category: category)
+    job5 = Job.create!(title: 'Developer',
+                       level_of_interest: 50,
+                       city: 'Denver',
+                       company: company2,
+                       category: category)
+    job6 = Job.create!(title: 'Developer',
+                       level_of_interest: 50,
+                       city: 'Seattle',
+                       company: company2,
+                       category: category)
+    # these three is for apple
+    job6 = Job.create!(title: 'Developer',
+                       level_of_interest: 60,
+                       city: 'Denver',
+                       company: company3,
+                       category: category)
+    job7 = Job.create!(title: 'Developer',
+                       level_of_interest: 60,
+                       city: 'Denver',
+                       company: company3,
+                       category: category)
+    job8 = Job.create!(title: 'Developer',
+                       level_of_interest: 60,
+                       city: 'Seattle',
+                       company: company3,
+                       category: category)
+
+    visit root_path
+    save_and_open_page
+    name3 = "#{company3.name} (3.0 stars)" # which should be apple should see #apple (3.0 stars)
+    name2 = "#{company2.name} (2.5 stars)" # which should be amazon should see amazon (2.4 stars) 
+    name1 = "#{company1.name} (2.0 stars)" # which should be turing should see turing (2.0 stars)
+
+    expect(page).to have_content('Top Companies by Interest')
+    expect(page).to have_content(name1)
+    expect(page).to have_content(name2)
+    expect(page).to have_content(name3)
   end
 end
